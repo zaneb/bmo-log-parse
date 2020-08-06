c3593aa25a1088724a0b67f834c2db20f81d48ed
Zane Bitter <zbitter@redhat.com>
2020-08-06 15:29:06 -0400

    Display paged output immediately when streaming
    If the output is streaming, do not try to display it without the pager
    if it is shorter than one screen, because the pager would wait for it to
    fill an entire screen before displaying anything.

--- bmo-log-parse.py
+++ bmo-log-parse.py
@@ -269,7 +269,10 @@ def pager(output_stream=None, line_buffer=False):
     import subprocess
     streams = {} if use_stdout else {'stdout': output_stream}
     streams['stdin'] = subprocess.PIPE
-    pager = subprocess.Popen(['less', '-R', '-F'], **streams)
+    args = ['--RAW-CONTROL-CHARS']  # Enable colour output
+    if not line_buffer:
+        args.append('--quit-if-one-screen')
+    pager = subprocess.Popen(['less'] + args, **streams)
     try:
         with io.TextIOWrapper(pager.stdin,
                               line_buffering=line_buffer,
