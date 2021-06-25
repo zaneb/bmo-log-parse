49ed8021484c38c92e74ff997c23a46b364bc6fe
Zane Bitter <zbitter@redhat.com>
2021-06-25 17:23:57 -0400

    Release version 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as readme:
 
 setuptools.setup(
     name='bmo-log-parse',
-    version='0.1.0',
+    version='0.1.1',
     author='Zane Bitter',
     author_email='zbitter@redhat.com',
     description='Utility for filtering and displaying logs from the Metal³ '
