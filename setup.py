533fd4bd72e102e75912b81b64b6b2554bcb732b
Zane Bitter <zbitter@redhat.com>
2021-06-29 15:48:33 -0400

    Release version 0.1.2
    This release fixes bugs with reading error logs from recent versions of
    baremetal-operator.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as readme:
 
 setuptools.setup(
     name='bmo-log-parse',
-    version='0.1.1',
+    version='0.1.2',
     author='Zane Bitter',
     author_email='zbitter@redhat.com',
     description='Utility for filtering and displaying logs from the Metal³ '
